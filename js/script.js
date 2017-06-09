47268548ff114ece365d74ecc2fc8f1c9d972c7b
Edvins Antonovs <ummahusla@gmail.com>
2017-06-09 09:55:32 +0100

    Updated naming, added couple of minor features

--- js/script.js
+++ js/script.js
@@ -1,18 +1,41 @@
 document.addEventListener('DOMContentLoaded', documentEvents, false);
 
-function getUserName(input) {
-    var name = input.value;
-    alert("The entered data is : " + input.value);
-    document.getElementById("user_name").innerHTML = name;
-    // TODO:
-    // Hide #user_name_wrap
-    // Save username to local storage / or sync
+function firstTimeUser(input) {
+  if (!input.value) {
+    console.error('Error: No value specified');
+    return;
+  }
+  var name = input.value;
+  console.log("The entered data is : " + name);
+  document.getElementById("user_name").innerHTML =  'Hello, ' + name;;
+  document.getElementById('user_name_wrap').style.display = 'none';
+
+  // Save it using the Chrome extension storage API.
+  chrome.storage.sync.set({'name': name}, function() {
+    console.log('User name is saved');
+  });
+
+  document.getElementById('user_metrics_wrap').style.display = 'inline';
+  getUserNameFromStorage();
 }
+
+function getUserNameFromStorage() {
+  chrome.storage.sync.get("name", function (obj) {
+    console.log(obj);
+  });
+}
+
+// TODO:
+// Write a function to check if there is a user name
+// and data saved in storage if so
+// show saved data from storage
+// otherwise show the UI with 
+
 
 function documentEvents() {
   document.getElementById('submit_user_name').addEventListener('click',
     function() {
-      getUserName(document.getElementById('user_name_input'));
+      firstTimeUser(document.getElementById('user_name_input'));
     }
   );
 }
